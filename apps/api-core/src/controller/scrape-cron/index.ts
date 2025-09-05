8f3e9ecef87e5f3e14bf911a96850384e40f9421
Alex <alexandre@frontierdental.com>
2025-09-05 10:02:31 -0400

    Fixing DataOnly save cron UI message bug

--- apps/api-core/src/controller/scrape-cron/index.ts
+++ apps/api-core/src/controller/scrape-cron/index.ts
@@ -7,7 +7,7 @@ import { runCron, runProduct } from "./run-cron";
 export const scrapeCronController = express.Router();
 
 scrapeCronController.get("/scrape/GetScrapeCron", startScrapeCron);
-scrapeCronController.get("/scrape/RecreateScrapeCron", recreateScrapeCron);
+scrapeCronController.post("/scrape/RecreateScrapeCron", recreateScrapeCron);
 scrapeCronController.post("/scrape/toggleCronStatus", toggleStatus);
 scrapeCronController.get("/scrape/run_soc/:cronName", runCron);
 scrapeCronController.get("/scrape/run_prod_soc/:product/:cronName", runProduct);
