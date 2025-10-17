9cbc14ebf4e820a0927119d483af75b31b93a7cf
RepricerSupport <repricer.support@voyantcs.com>
2025-10-17 16:12:25 +0530

    Added Logic for Slow Cron Product Check and made monitor API without authentication

--- apps/repricer/src/routes/monitor.ts
+++ apps/repricer/src/routes/monitor.ts
@@ -4,14 +4,19 @@ import { authMiddleware } from "../middleware/auth-middleware";
 
 export const monitorRouter = express.Router();
 
-monitorRouter.use(authMiddleware);
+//monitorRouter.use(authMiddleware);
 
 monitorRouter.get("/get_cron_details", monitorController.GetInprogressCron);
 monitorRouter.get("/get_422_product", monitorController.Get422ProductDetails);
 
-monitorRouter.get("/get_floor_below", monitorController.GetProductsBelowFloor);
+monitorRouter.get(
+  "/get_floor_below",
+  authMiddleware,
+  monitorController.GetProductsBelowFloor,
+);
 
 monitorRouter.get(
   "/load_scrape_only/:pageNo/:pageSize",
+  authMiddleware,
   monitorController.LoadScrapeOnlyProducts,
 );
