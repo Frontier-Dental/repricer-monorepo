573d434d57add5cbff5b5c081fbd6d1f46a90399
RepricerSupport <repricer.support@voyantcs.com>
2025-11-21 02:17:59 +0530

    Changes for Streaming Excel without loading onto Memory

--- apps/excel-export/src/utility/mapper/mysql-mapper.ts
+++ apps/excel-export/src/utility/mapper/mysql-mapper.ts
@@ -16,77 +16,83 @@ export function MapProductDetailsList(payload: any) {
     return mappedList;
   }
   for (const prodId of listOfProductIds) {
-    const mappedProduct = {
-      mpId: prodId,
-      isSlowActivated:
-        _.first(groupedList[parseInt(prodId)]).IsSlowActivated == 1
-          ? true
-          : false,
-      isScrapeOnlyActivated:
-        _.first(groupedList[parseInt(prodId)]).ScrapeOnlyActive == 1
-          ? true
-          : false,
-      scrapeOnlyCronId: _.first(groupedList[parseInt(prodId)])
-        .LinkedScrapeOnlyCronId,
-      scrapeOnlyCronName: _.first(groupedList[parseInt(prodId)])
-        .LinkedScrapeOnlyCron,
-      isBadgeItem:
-        _.first(groupedList[parseInt(prodId)]).IsBadgeItem == 1 ? true : false,
-      cronId: _.first(groupedList[parseInt(prodId)]).RegularCronId,
-      net32url: _.first(groupedList[parseInt(prodId)]).Net32Url,
-      slowCronId: _.first(groupedList[parseInt(prodId)]).SlowCronId,
-      algo_execution_mode: _.first(groupedList[parseInt(prodId)])
-        .algo_execution_mode,
-      tradentLinkInfo: getLinkedInfoForVendor(
-        groupedList[parseInt(prodId)],
-        VendorName.TRADENT,
-      ),
-      frontierLinkInfo: getLinkedInfoForVendor(
-        groupedList[parseInt(prodId)],
-        VendorName.FRONTIER,
-      ),
-      mvpLinkInfo: getLinkedInfoForVendor(
-        groupedList[parseInt(prodId)],
-        VendorName.MVP,
-      ),
-      topDentLinkInfo: getLinkedInfoForVendor(
-        groupedList[parseInt(prodId)],
-        VendorName.TOPDENT,
-      ),
-      firstDentLinkInfo: getLinkedInfoForVendor(
-        groupedList[parseInt(prodId)],
-        VendorName.FIRSTDENT,
-      ),
-      triadLinkInfo: getLinkedInfoForVendor(
-        groupedList[parseInt(prodId)],
-        VendorName.TRIAD,
-      ),
-      tradentDetails: getMappedVendorDetails(
-        groupedList[parseInt(prodId)],
-        VendorName.TRADENT,
-      ),
-      frontierDetails: getMappedVendorDetails(
-        groupedList[parseInt(prodId)],
-        VendorName.FRONTIER,
-      ),
-      mvpDetails: getMappedVendorDetails(
-        groupedList[parseInt(prodId)],
-        VendorName.MVP,
-      ),
-      topDentDetails: getMappedVendorDetails(
-        groupedList[parseInt(prodId)],
-        VendorName.TOPDENT,
-      ),
-      firstDentDetails: getMappedVendorDetails(
-        groupedList[parseInt(prodId)],
-        VendorName.FIRSTDENT,
-      ),
-      triadDetails: getMappedVendorDetails(
-        groupedList[parseInt(prodId)],
-        VendorName.TRIAD,
-      ),
-    };
-    mappedList.push(mappedProduct as any);
+    try {
+      const mappedProduct = {
+        mpId: prodId,
+        isSlowActivated:
+          _.first(groupedList[parseInt(prodId)]).IsSlowActivated == 1
+            ? true
+            : false,
+        isScrapeOnlyActivated:
+          _.first(groupedList[parseInt(prodId)]).ScrapeOnlyActive == 1
+            ? true
+            : false,
+        scrapeOnlyCronId: _.first(groupedList[parseInt(prodId)])
+          .LinkedScrapeOnlyCronId,
+        scrapeOnlyCronName: _.first(groupedList[parseInt(prodId)])
+          .LinkedScrapeOnlyCron,
+        isBadgeItem:
+          _.first(groupedList[parseInt(prodId)]).IsBadgeItem == 1
+            ? true
+            : false,
+        cronId: _.first(groupedList[parseInt(prodId)]).RegularCronId,
+        net32url: _.first(groupedList[parseInt(prodId)]).Net32Url,
+        slowCronId: _.first(groupedList[parseInt(prodId)]).SlowCronId,
+        algo_execution_mode: _.first(groupedList[parseInt(prodId)])
+          .algo_execution_mode,
+        tradentLinkInfo: getLinkedInfoForVendor(
+          groupedList[parseInt(prodId)],
+          VendorName.TRADENT,
+        ),
+        frontierLinkInfo: getLinkedInfoForVendor(
+          groupedList[parseInt(prodId)],
+          VendorName.FRONTIER,
+        ),
+        mvpLinkInfo: getLinkedInfoForVendor(
+          groupedList[parseInt(prodId)],
+          VendorName.MVP,
+        ),
+        topDentLinkInfo: getLinkedInfoForVendor(
+          groupedList[parseInt(prodId)],
+          VendorName.TOPDENT,
+        ),
+        firstDentLinkInfo: getLinkedInfoForVendor(
+          groupedList[parseInt(prodId)],
+          VendorName.FIRSTDENT,
+        ),
+        triadLinkInfo: getLinkedInfoForVendor(
+          groupedList[parseInt(prodId)],
+          VendorName.TRIAD,
+        ),
+        tradentDetails: getMappedVendorDetails(
+          groupedList[parseInt(prodId)],
+          VendorName.TRADENT,
+        ),
+        frontierDetails: getMappedVendorDetails(
+          groupedList[parseInt(prodId)],
+          VendorName.FRONTIER,
+        ),
+        mvpDetails: getMappedVendorDetails(
+          groupedList[parseInt(prodId)],
+          VendorName.MVP,
+        ),
+        topDentDetails: getMappedVendorDetails(
+          groupedList[parseInt(prodId)],
+          VendorName.TOPDENT,
+        ),
+        firstDentDetails: getMappedVendorDetails(
+          groupedList[parseInt(prodId)],
+          VendorName.FIRSTDENT,
+        ),
+        triadDetails: getMappedVendorDetails(
+          groupedList[parseInt(prodId)],
+          VendorName.TRIAD,
+        ),
+      };
+      mappedList.push(mappedProduct as any);
+    } catch (err) {
+      console.error(`Exception while mapping ${prodId} || ${err}`);
+    }
   }
   return mappedList;
 }
