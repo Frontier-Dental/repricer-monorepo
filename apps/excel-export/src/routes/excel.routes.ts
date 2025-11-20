07e252cf6b65a296b11400cd19770eb87e21eaf1
RepricerSupport <repricer.support@voyantcs.com>
2025-11-21 02:29:27 +0530

    Merge pull request #66 from Frontier-Dental/feature/modify-export-excel
    Changes for Streaming Excel without loading onto Memory
    
    (cherry picked from commit 14cbf568d1c8ce1d7816356da1f3eca9dfe9cb59)

--- apps/excel-export/src/routes/excel.routes.ts
+++ apps/excel-export/src/routes/excel.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import * as excelController from "../controllers/excel.controller";
+import * as excelStreamController from "../controllers/excel.stream.controller";
 
 export const excelRouter = Router();
 
 excelRouter.get("/download", excelController.exportItems);
+excelRouter.get(
+  "/download/all_items",
+  excelStreamController.streamProductDetails,
+);
