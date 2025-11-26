bbd12d55894e40d39e07a8ee343da74071eb60e7
apple <apple@apples-MacBook-Pro.local>
2025-11-26 13:47:48 +0100

    feat: Setup model and migration for waitlist table

---
+++ apps/api-core/src/model/waitlist-model.ts
@@ -0,0 +1,33 @@
+export class WaitlistModel {
+  mp_id: number;
+  vendor_name: string;
+  old_inventory: number;
+  new_inventory: number;
+  net32_inventory: number;
+  api_status?: string;
+  message?: string;
+  created_at?: Date;
+  updated_at?: Date;
+
+  constructor(
+    mp_id: number,
+    vendor_name: string,
+    old_inventory: number,
+    new_inventory: number,
+    net32_inventory: number,
+    api_status?: string,
+    message?: string,
+    created_at?: Date,
+    updated_at?: Date,
+  ) {
+    this.mp_id = mp_id;
+    this.vendor_name = vendor_name;
+    this.old_inventory = old_inventory;
+    this.new_inventory = new_inventory;
+    this.net32_inventory = net32_inventory;
+    this.api_status = api_status;
+    this.message = message;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+  }
+}
