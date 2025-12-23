2d41d1b8a99858d45c9d41dcab3b7d84f6ece428
RepricerSupport <repricer.support@voyantcs.com>
2025-12-24 01:25:24 +0530

    Reverted the Code for DB Call for Error Items

--- apps/api-core/src/utility/mongo/db-helper.ts
+++ apps/api-core/src/utility/mongo/db-helper.ts
@@ -233,11 +233,6 @@ export async function GetEligibleContextErrorItems(_activeStatus: any, _mpId: an
         mpId: parseInt(_mpId),
       },
       {
-        nextCronTime: {
-          $lte: new Date(),
-        },
-      },
-      {
         vendorName: {
           $ne: _contextVendor,
         },
