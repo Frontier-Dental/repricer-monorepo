970f73a5d51422cbe6660d1da3e1cd81779a162f
Peter <peter@executivepros.co>
2026-03-17 16:42:19 +0100

    chore: Fix test

--- apps/excel-export/jest.config.cjs
+++ apps/excel-export/jest.config.cjs
@@ -3,16 +3,24 @@ const path = require("path");
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["<rootDir>/src/**/?(*.)+(spec|test).ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  modulePathIgnorePatterns: ["/dist/"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
       { tsconfig: path.join(__dirname, "tsconfig.jest.json") },
     ],
   },
-  testMatch: ["<rootDir>/src/**/__tests__/**/*.test.ts", "<rootDir>/src/**/*.test.ts"],
-  collectCoverageFrom: ["src/**/*.{ts,js}", "!src/**/*.d.ts", "!src/**/*.test.ts", "!src/**/*.spec.ts", "!src/main.ts", "!src/**/__tests__/**", "!src/**/__mocks__/**"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,js}",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.ts",
+    "!src/**/*.spec.ts",
+    "!src/main.ts",
+    "!src/**/__tests__/**",
+    "!src/**/__mocks__/**",
+  ],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html", "json-summary"],
   coverageThreshold: {
